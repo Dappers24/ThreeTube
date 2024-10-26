1088c158ab4f7705bd0f8a779b646bf5456cf18f
sgcodes7471 <official06srinjoy@gmail.com>
2024-10-26 18:23:38 +0530

    FFMPEG intregation

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import bodyParser from 'body-parser';
 import keys from './configs/server_config.js';
 import { fetchFileFromIFPS, uploadFileToIPFS } from './services/pinata_services.js';
+import { segmentVideo } from './services/ffmpeg_services.js';
 const app = express()
 
 app.use(
@@ -19,9 +20,16 @@ app.use(bodyParser.text());
 app.post('/upload', async (req, res)=>{
     try{
         const {title , description , tags} = req.body
-        //ffmpeg segments the video
+
+        //ffmpeg segments the video in the segmentVideo() function
+        // const inputPath = './video.mp4'
+        // const outputFolder = './videos'
+        // const segmentation = await segmentVideo(inputPath , outputFolder)
+        // if(!segmentation) res.status(500).json({error:true,message:'File upload failed'})
+
         const response = await uploadFileToIPFS({filePath:'./ss2.png' , metadata:{title:title , description:description , tags:tags}})
         if(!response) return res.status(500).json({error:true,message:'File upload failed'})
+        
         //sending the frontend response to contact with the smart contracts to store the returned ipfsHash in ethereum blockchain via subgraphs
         return res.status(200).json({error:false,message:'File upload success'})
     }catch(error){
