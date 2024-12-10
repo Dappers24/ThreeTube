4b764d1c56150c2527fa93961036da1f5432e3ba
sgcodes7471 <official06srinjoy@gmail.com>
2024-12-11 00:15:13 +0530

    Initialize subgraph

---
+++ threetube/src/threetube.ts
@@ -0,0 +1,70 @@
+import {
+  VideoAdded as VideoAddedEvent,
+  VideoLiked as VideoLikedEvent,
+  VideoUnLiked as VideoUnLikedEvent,
+  VideoViewed as VideoViewedEvent
+} from "../generated/threetube/threetube"
+import {
+  VideoAdded,
+  VideoLiked,
+  VideoUnLiked,
+  VideoViewed
+} from "../generated/schema"
+
+export function handleVideoAdded(event: VideoAddedEvent): void {
+  let entity = new VideoAdded(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.videoId = event.params.videoId
+  entity.cid = event.params.cid
+  entity.metadata = event.params.metadata
+  entity.owner = event.params.owner
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleVideoLiked(event: VideoLikedEvent): void {
+  let entity = new VideoLiked(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.videoId = event.params.videoId
+  entity.likes = event.params.likes
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleVideoUnLiked(event: VideoUnLikedEvent): void {
+  let entity = new VideoUnLiked(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.videoId = event.params.videoId
+  entity.likes = event.params.likes
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleVideoViewed(event: VideoViewedEvent): void {
+  let entity = new VideoViewed(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.videoId = event.params.videoId
+  entity.views = event.params.views
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
---
+++ threetube/tests/threetube.test.ts
@@ -0,0 +1,72 @@
+import {
+  assert,
+  describe,
+  test,
+  clearStore,
+  beforeAll,
+  afterAll
+} from "matchstick-as/assembly/index"
+import { BigInt, Address } from "@graphprotocol/graph-ts"
+import { VideoAdded } from "../generated/schema"
+import { VideoAdded as VideoAddedEvent } from "../generated/threetube/threetube"
+import { handleVideoAdded } from "../src/threetube"
+import { createVideoAddedEvent } from "./threetube-utils"
+
+// Tests structure (matchstick-as >=0.5.0)
+// https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
+
+describe("Describe entity assertions", () => {
+  beforeAll(() => {
+    let videoId = BigInt.fromI32(234)
+    let cid = "Example string value"
+    let metadata = "Example string value"
+    let owner = Address.fromString("0x0000000000000000000000000000000000000001")
+    let newVideoAddedEvent = createVideoAddedEvent(
+      videoId,
+      cid,
+      metadata,
+      owner
+    )
+    handleVideoAdded(newVideoAddedEvent)
+  })
+
+  afterAll(() => {
+    clearStore()
+  })
+
+  // For more test scenarios, see:
+  // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+
+  test("VideoAdded created and stored", () => {
+    assert.entityCount("VideoAdded", 1)
+
+    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+    assert.fieldEquals(
+      "VideoAdded",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "videoId",
+      "234"
+    )
+    assert.fieldEquals(
+      "VideoAdded",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "cid",
+      "Example string value"
+    )
+    assert.fieldEquals(
+      "VideoAdded",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "metadata",
+      "Example string value"
+    )
+    assert.fieldEquals(
+      "VideoAdded",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "owner",
+      "0x0000000000000000000000000000000000000001"
+    )
+
+    // More assert options:
+    // https://thegraph.com/docs/en/developer/matchstick/#asserts
+  })
+})
